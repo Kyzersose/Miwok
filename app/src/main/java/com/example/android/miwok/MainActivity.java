1a42856994184f2177a078803410274f56286a3b
shaefferm <matt@theshaeffers.com>
2016-08-30 16:35:36 -0500

    d

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -21,8 +21,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -31,11 +29,47 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNumbersList(View view){
-        //TODO code the Intent to open the numbers activity
-        Intent intent = new Intent(this, NumbersActivity.class);
-        startActivity(intent);
+
+        TextView numbersView = (TextView) findViewById(R.id.numbers);
+        numbersView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NumbersActivity.class));
+
+            }
+        });
+
+        TextView colorsView = (TextView) findViewById(R.id.colors);
+        colorsView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ColorActivity.class));
+
+            }
+        });
+
+        TextView familyView = (TextView) findViewById(R.id.family);
+        familyView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, FamilyActivity.class));
+
+            }
+        });
+
+        TextView phrasesView = (TextView) findViewById(R.id.phrases);
+        phrasesView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PhrasesActivity.class));
+
+            }
+        });
+
     }
 }
